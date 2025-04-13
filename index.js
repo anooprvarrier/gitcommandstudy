515e92f51307ab137d79d612af830b46b4ec3f5a
Anoop Rajasekhara Warrier <anooprvarrier@zoho.com>
2025-04-13 11:21:33 +1000

    feat(git) - This is the initial commit with just package.json, index.js & an Readme.md file

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log(
+  `Welcome to world of GIT !!! Here we could learn and grow & wishing you a happy learning and a happy financial year 2025-26 !!!`
+);
+console.log(`Let's get started !!!`);
