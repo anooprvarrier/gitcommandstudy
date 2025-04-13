b874a5f7c5d43c8a1935dc2f8055b27d6d24bcda
Anoop Rajasekhara Warrier <anooprvarrier@zoho.com>
2025-04-13 11:33:46 +1000

    feat(git) - Added nodemon package, changed start command and added .gitignore file with few items which is to be ignored

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 console.log(
   `Welcome to world of GIT !!! Here we could learn and grow & wishing you a happy learning and a happy financial year 2025-26 !!!`
 );
+
 console.log(`Let's get started !!!`);
+
+console.log(
+  `Adding nodemon package. nodemon is a tool that helps develop Node.js based applications by automatically restarting the node application when file changes in the directory are detected.`
+);
+
+console.log(
+  `Modified the start command. Now whenever we make some changes in index.js file or any other used files, the nodemon will automatically restart the server. This will come handy during our development activities. So lets try by saving the file now.`
+);
